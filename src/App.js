dc6f81b5c067029ed86a16db63afe22178bd79d9
Oliver <oliver.a.jarjosa@gmail.com>
2024-03-19 15:05:38 -0400

    models

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
+
+// pages & components
+import Home from './pages/Home.js'
+import Navbar from './components/Navbar'
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -23,3 +30,5 @@ function App() {
 }
 
 export default App;
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
---
+++ src/components/Navbar.js
@@ -0,0 +1,34 @@
+import { Link } from 'react-router-dom';
+
+
+
+
+const Navbar = () => {
+
+  return (
+    <header>
+      <div className="container">
+        <Link to="/">
+          <h1>Home</h1>
+        </Link>
+        <Link to="/">
+          <h1>About Us</h1>
+        </Link>
+        <Link to="/">
+          <h1>Animal Adoption</h1>
+        </Link>
+        <Link to="/">
+          <h1>Shop</h1>
+        </Link>
+        <Link to="/">
+          <h1>Contact</h1>
+        </Link>
+        <Link to="/">
+          <h1>Register</h1>
+        </Link>
+      </div>
+    </header>
+  )
+}
+
+export default Navbar
