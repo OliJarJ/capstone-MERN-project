2d2feccb9fd1428e047599b56b648ef9effd4cd3
Abigail Collins <abigailjcollins@gmail.com>
2024-03-19 16:09:10 -0400

    Routes and controllers added

---
+++ backend/controllers/shopcontrollers.js
@@ -0,0 +1,26 @@
+//get all pets
+const getPets = async (req, res) => {
+    const pets = await Pets.find({}).sort({createdAt: -1}) //so that newest pets are at the top
+    res.status(200).json(pets)
+}
+
+
+// create new pet
+const createPet = async (req, res) =>{
+    const {title, description, category, price, date_added} = req.body
+
+    // add doc to db
+    try{
+        const pet = await pet.create({title, description, category, price, date_added})
+        res.status(200).json(pet)
+    } catch(error){
+        res.status(400).json({error: error.message})
+    }
+}
+
+
+module.exports = {
+    createPet,
+    getPets,
+   
+}
